77c9ea654e03f5decbf28ccedfb4891541df499d
S-Guettner <sven_guettner@protonmail.com>
2023-09-23 17:00:50 +0200

    db connection

---
+++ app/DB/dbConnection.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+
+let isConnected = false
+
+const connectionToDB = async () => {
+    
+
+    if (isConnected) {
+        console.log("DB is connected")
+        return
+    }
+
+    try {
+        await mongoose.connect(process.env.DATABASE_URL as string , {
+            dbName: "domain-dash",
+        })
+        isConnected = true
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
+
+export default connectionToDB
