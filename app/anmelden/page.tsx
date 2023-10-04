5ec368b143425d4650053ba84b15b0f04fe279e7
S-Guettner <sven_guettner@protonmail.com>
2023-10-04 18:38:47 +0200

    added login modal

--- app/anmelden/page.tsx
+++ app/anmelden/page.tsx
@@ -1,11 +1,13 @@
+'use client'
 import { FC } from "react";
+import LoginModalCustomer from "../components/customer/LoginModalCustomer";
 
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
   return (
     <main>
-        anmelden
+        <LoginModalCustomer />
     </main>
   )
 };
